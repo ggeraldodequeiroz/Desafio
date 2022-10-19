0e6c24a9849b3a0820c0b95fa6660fb7137ca1d1
Guilherme Queiroz <ggeraldodequeiroz@outlook.com>
2022-10-18 21:00:48 -0300

    Criando configurações para salvar no banco de dados.

--- backend-test/api/alunos.js
+++ backend-test/api/alunos.js
@@ -20,6 +20,28 @@ module.exports = (app) => {
         
         const aluno = { ...req.body };
 
+        if(!aluno.registroAcademico){
+            return res.json({error: "Registro acadêmico é um campo obrigatório"});
+        }
+
+        if(!aluno.nome){
+            return res.json({error: "Nome é um campo obrigatório"});
+        }
+
+        if(!aluno.email){
+            return res.json({error: "E-mail é um campo obrigatório"});
+        }
+
+        if(!aluno.cpf){
+            return res.json({error: "CPF é um campo obrigatório"});
+        }
+
+        app
+            .database("alunos")
+            .insert(aluno)
+            .then(() => res.status(200).send())
+            .catch((err) => res.status(500).send(err));
+
         return res.json(aluno);
     }
 
--- backend-test/index.js
+++ backend-test/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const consign = require("consign");
+const database = require("./config/database")
 
 app.use(express.json())
+app.database = database;
 
 consign()
     .then("./api")
