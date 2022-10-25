10dc0416e9a1124675d5f4f5552576318282bfde
ggeraldodequeiroz <ggeraldodequeiroz@outlook.com>
2022-10-25 15:51:51 -0300

    Atualizado pasta com os projetos recentes.

--- backend-desafio/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3304;
-
-app.listen(PORT, () => console.log(`API Desafio Grupo A running or port ${PORT}`));
